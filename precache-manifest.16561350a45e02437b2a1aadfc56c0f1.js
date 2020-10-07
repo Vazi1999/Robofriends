c4c25706ae9197d9f66a4789b9300af726fbf1ac
Vazi1999 <71448796+Vazi1999@users.noreply.github.com>
2020-10-07 13:05:34 +0300

    Updates

---
+++ precache-manifest.16561350a45e02437b2a1aadfc56c0f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9d4308f5f4186293b068595783bc00",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a7b2d2b0a3150fa0152c",
+    "url": "/RoboFriends/static/css/main.bb91abd0.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b2d2b0a3150fa0152c",
+    "url": "/RoboFriends/static/js/main.975071df.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
